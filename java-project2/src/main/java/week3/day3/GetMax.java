61c7ce7af2b4c0748504954bfb8616579e7e9722
bluelily555 <bluelily555@daum.net>
2023-05-04 11:51:20 +0900

    Java Example 최댓값, 최솟값 구하기 (week3_day4)

---
+++ java-project2/src/main/java/week3/day3/GetMax.java
@@ -0,0 +1,13 @@
+package week3.day3;
+
+public class GetMax {
+    public static void main(String[] args) {
+        int[] arr= {2, 1, 31, 9, 7};;
+        int targetValue = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+            if(targetValue < arr[i]) targetValue = arr[i];
+        }
+        System.out.println(targetValue);
+    }
+}
