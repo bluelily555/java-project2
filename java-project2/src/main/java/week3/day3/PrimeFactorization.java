f32d9bb30a1ca7aa3dd59008b68d632b2c371a51
bluelily555 <bluelily555@daum.net>
2023-05-04 09:21:30 +0900

    Java Example 도전과제 소인수분해 - int배열만 사용 (week3_day3)

--- java-project2/src/main/java/week3/day3/PrimeFactorization.java
+++ java-project2/src/main/java/week3/day3/PrimeFactorization.java
@@ -1,26 +1,40 @@
 package week3.day3;
 
+import java.util.Arrays;
+
 public class PrimeFactorization {
     public static void main(String[] args) {
         int n1 = 12;
-        System.out.println(Solution.solution(n1));
+        System.out.println(Arrays.toString(Solution.solution(n1)));
         System.out.println();
         int n2 = 17;
-        System.out.println(Solution.solution(n2));
+        System.out.println(Arrays.toString(Solution.solution(n2)));
         System.out.println();
         int n3 = 420;
-        System.out.println(Solution.solution(n3));
+        System.out.println(Arrays.toString(Solution.solution(n3)));
     }
 }
 
 class Solution {
     public static int[] solution(int n) {
-        int cnt = 0;
+        int[] tempArr = new int[n];
         for (int i = 2; i <= n ; i++) {
-            if(n % i == 0) cnt++;
+            while (n % i == 0) {
+                tempArr[i-1] = i;
+                n /= i;
+            }
+        }
+
+        int cnt = 0;
+        for (int i = 0; i < tempArr.length; i++) {
+            if(tempArr[i] > 0) cnt++;
         }
         int[] answer = new int[cnt];
 
+        int idx = 0;
+        for (int i = 0; i < tempArr.length; i++) {
+            if(tempArr[i] != 0) answer[idx++] = tempArr[i];
+        }
 
         return answer;
     }
