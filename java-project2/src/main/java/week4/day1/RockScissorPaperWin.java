df74d17ff7244d87d5808e8acdf1f332847b2883
bluelily555 <bluelily555@daum.net>
2023-05-08 15:24:47 +0900

    도전과제 이길때까지 하는 가위바위보 게임 (week4_day1)

---
+++ java-project2/src/main/java/week4/day1/RockScissorPaperWin.java
@@ -0,0 +1,25 @@
+package week4.day1;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class RockScissorPaperWin {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+
+        boolean win = false;
+        do{
+            int me = sc.nextInt();
+            int computer = rand.nextInt(3);
+
+            System.out.println("컴퓨터: " + computer);
+            if(me - computer == 0) System.out.println("tie");
+            else if (me - computer == -1 || me - computer == 2){
+                System.out.println("win");
+                win = true;
+            } else System.out.println("lose");
+        } while(!win);
+    }
+
+}
